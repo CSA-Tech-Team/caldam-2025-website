9bf6613dfd41b3401dd9618367ad18d205a0768c
thatcatfromspace <encyclopedia748@gmail.com>
2024-03-08 08:39:11 +0530

    landing initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,11 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      "magenta": '#932F6D',
+      "seasalt": '#F7F7F9',
     },
+    extend: {}
   },
   plugins: [],
 };
