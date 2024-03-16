7be9c691414d8f71750c6182b1918d76b88c7ddb
Rahul-02825 <rahul2825.kannan@gmail.com>
2024-03-16 23:12:50 +0530

    Timer component is added in page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,13 @@
 import HomeComponent from "@/components/UI/Home";
 import PreConferenceSchoolLinksComponent from "@/components/UI/PreConferenceSchollLinks";
+import TimerComponent from "@/components/UI/Timer";
+
 
 export default function Home() {
     return (
         <div className="">
             <HomeComponent />
+            <TimerComponent />
             <PreConferenceSchoolLinksComponent />
         </div>
     );
