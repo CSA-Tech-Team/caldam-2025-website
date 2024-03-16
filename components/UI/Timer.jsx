a642f8577ab52aa2c6802180a5022532abb74ea2
Rahul-02825 <rahul2825.kannan@gmail.com>
2024-03-16 23:10:30 +0530

    Timer component written

---
+++ components/UI/Timer.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+
+function Timer() {
+  const calculateTimeLeft = () => {
+    const difference = +new Date('2024-12-31') - +new Date();
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
+  const addLeadingZero = (value) => {
+    return value < 10 ? `0${value}` : value;
+  };
+
+  const { days, hours, minutes } = timeLeft;
+
+  return (
+    <div className="bg-sky-950 text-white text-center py-4 text-6xl h-28 pt-6 font-bold">
+      <div className="flex justify-center">
+        {days > 0 && <span className="mr-2">{`${days}  :`}</span>}
+        <span>{`${addLeadingZero(hours)} : ${addLeadingZero(minutes)}`}</span>
+      </div>
+    </div>
+  );
+}
+
+export default Timer;
