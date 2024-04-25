5dd94d67064a83929676847759638cc1498f53b8
MohamedAklamaash <aklamaash78@gmail.com>
2024-04-25 14:02:22 +0530

    rectified build error

--- app/accepted-papers/page.jsx
+++ app/accepted-papers/page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Proceedings from "@/components/accepted-papers/components/Proceedings";
 import Acceptedpapers from "@/components/accepted-papers/components/Acceptedpapers";
 
-export default function AcceptedPapers() {
+export default function AcceptedPaper() {
   return (
     <div className="container mx-auto px-4">
       <Proceedings />
