0382021f901d07a328df87fc1725aff380e59084
MohamedAklamaash <aklamaash78@gmail.com>
2024-05-04 17:01:03 +0530

    updated footer

--- components/shared/re-used/Footer.jsx
+++ components/shared/re-used/Footer.jsx
@@ -15,24 +15,17 @@ function Footer() {
       <div
         className={`relative mb-8 flex justify-between max-lg:flex-col max-lg:space-y-10`}
       >
-        {path === "/" && (
-          <div>
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1958.0951362514777!2d77.00151924190861!3d11.024346534066895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba8582f1435fa59%3A0x137d95bfd8909293!2sPSG%20College%20Of%20Technology!5e0!3m2!1sen!2sin!4v1710593008393!5m2!1sen!2sin"
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-              allowFullScreen=""
-              className="h-full w-full"
-            ></iframe>
-          </div>
-        )}
-        <div
-          className={`lg:text-right ${path !== "/" && "flex flex-1 justify-between max-lg:flex-col"} 
-        ${path === "/call-for-papers" && "mx-[40%] flex  items-center justify-center  "}`}
-        >
-          <div
-            className={`mb-8 text-lg ${path === "/call-for-papers" && "flex h-full flex-col items-end justify-between"} `}
-          >
+        <div>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1958.0951362514777!2d77.00151924190861!3d11.024346534066895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba8582f1435fa59%3A0x137d95bfd8909293!2sPSG%20College%20Of%20Technology!5e0!3m2!1sen!2sin!4v1710593008393!5m2!1sen!2sin"
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            allowFullScreen=""
+            className="h-full w-full"
+          ></iframe>
+        </div>
+        <div className={`lg:text-right `}>
+          <div className={`mb-8 text-lg  `}>
             <h1 className="text-2xl font-bold">CONTACT</h1>
             <p>{footerData["contactEmail"]}</p>
             <p>{footerData["contactMobile"]}</p>
