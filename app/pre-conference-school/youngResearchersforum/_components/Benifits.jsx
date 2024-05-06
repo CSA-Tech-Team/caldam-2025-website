c2917c7f4b34b99909f9a37912cea14efb2980f4
MohamedAklamaash <aklamaash78@gmail.com>
2024-05-06 12:57:49 +0530

    updated style

--- app/pre-conference-school/youngResearchersforum/_components/Benifits.jsx
+++ app/pre-conference-school/youngResearchersforum/_components/Benifits.jsx
@@ -6,12 +6,12 @@ export default function Benifits() {
       <h1 className='text-3xl lg:text-4xl 2xl:text-5xl italic font-bold'>
         Benifits
       </h1>
-      <ul className='leading-tight space-y-5 py-4 '>
+      <ul className='leading-tight space-y-5 px-3 py-4 '>
         {benifits.map((benifit, i) => {
           return (
             <li
               key={i}
-              className="before:content-[' '] text-lg relative pl-6 before:absolute before:left-0 before:top-[5px] before:m-auto before:h-4 before:w-4 before:bg-orange"
+              className="before:content-[' '] text-lg relative pl-6 before:absolute before:left-0 before:top-[5px] before:m-auto before:h-4 before:w-4 before:rounded-full before:bg-orange"
             >
               {benifit.benifit}
             </li>
--- app/pre-conference-school/youngResearchersforum/_components/Eligiblity.jsx
+++ app/pre-conference-school/youngResearchersforum/_components/Eligiblity.jsx
@@ -6,12 +6,12 @@ export default function Eligiblity() {
       <h1 className='text-3xl lg:text-4xl 2xl:text-5xl italic font-bold'>
         Eligiblity
       </h1>
-      <ul className='leading-tight space-y-5 py-5 '>
+      <ul className='leading-tight space-y-5 px-3 py-5 '>
         {eligiblities.map((eligiblity, i) => {
           return (
             <li
               key={i}
-              className="before:content-[' '] text-lg relative pl-6 before:absolute before:left-0 before:top-[5px] before:m-auto before:h-4 before:w-4 before:bg-orange"
+              className="before:content-[' '] text-lg relative pl-6 before:absolute before:rounded-full before:left-0 before:top-[5px] before:m-auto before:h-4 before:w-4 before:bg-orange"
             >
               {eligiblity.eligiblity}
             </li>
