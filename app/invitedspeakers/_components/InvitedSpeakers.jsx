ae6009393a72db2d7572268d8fd097e15e8627e3
MohamedAklamaash <aklamaash78@gmail.com>
2024-07-15 12:41:40 +0530

    added a invited speaker

--- app/invitedspeakers/_components/InvitedSpeakers.jsx
+++ app/invitedspeakers/_components/InvitedSpeakers.jsx
@@ -1,40 +1,44 @@
 import React from "react";
 import invitedspeakers from "@/constants/invited-speakers.json";
-
+import Link from "next/link";
+import { BiLink } from "react-icons/bi";
 function InvitedSpeaker() {
   return (
     <div className="bg-bluecolor p-8 lg:p-14">
       <h1 className="mb-10 text-4xl font-bold uppercase text-white">
-        Invited Speakers
+        Tentative List Of Invited Speakers
       </h1>
-      <div className="w-full flex justify-center items-center">
+      {/* <div className="w-full flex justify-center items-center">
         <h2 className="text-2xl text-white font-semibold"> TBA </h2>
-      </div>
-      {/* <div className="grid grid-cols-3 gap-5 text-white max-lg:grid-cols-2 max-md:grid-cols-1">
-         {invitedspeakers.map((invitedSpeaker, index) => {
+      </div> */}
+      <div className="grid grid-cols-3 gap-5 text-white max-lg:grid-cols-2 max-md:grid-cols-1">
+        {invitedspeakers.map((invitedSpeaker, index) => {
           return (
             <div
               key={index}
               className="flex flex-col items-center justify-center space-y-5 rounded-lg bg-white px-6 py-6 text-black"
             >
-              <h1 className="text-2xl font-bold lg:text-3xl">
-                {invitedSpeaker.name}
-              </h1>
+              <Link href={invitedSpeaker.link} className="flex items-center justify-center" >
+                <BiLink />
+                <h1 className="text-2xl font-bold lg:text-3xl">
+                  {invitedSpeaker.name}
+                </h1>
+              </Link>
               <h1 className=" text-md lg:text-lg">
                 {invitedSpeaker.collegeName}
               </h1>
-              <div className=" h-0.5 w-full border border-b-2 border-black " />
+              {/* <div className=" h-0.5 w-full border border-b-2 border-black " />
               <h1 className="text-lg lg:text-2xl">
                 Topic:{invitedSpeaker.topic}
               </h1>
               // following single line comments are OPTIONAL!
               //  <button className=" rounded-md bg-orange px-8 py-3 font-semibold text-black ">
               //   Go to Slides {"> >"}
-              // </button> }
+              // </button>  */}
             </div>
           );
         })}
-      </div> */}
+      </div>
     </div>
   );
 }
